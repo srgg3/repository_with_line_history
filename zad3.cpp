27b6b1a0620db48605f69380b3b2b48575991c3b
srgg3 <Smirnovsd2405@yandex.ru>
2026-03-27 23:14:09 +0400

    Добавим функцию fib(n)

--- zad3.cpp
+++ zad3.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+void fib(int n){
+    int first = 1;
+    int second = 1;
+    vector <int> nums;
+    nums.push_back(first);
+    if (n == 1){
+        cout << nums.back();
+    }
+    nums.push_back(second);
+    if (n == 2){
+        cout << nums.back();
+    }
+    while (nums.size() < n){
+        nums.push_back(nums[nums.size() - 2] + nums[nums.size() - 1]);
+    }
+    cout << nums.back();
+}
+
 int main(){
     cout << "Hello, World!" << endl;
+    fib(10);
 }
